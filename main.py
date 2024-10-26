1f5fa95bc681c8bd2cb5b3aa45a882899a5bb2db
petr-khul <petr.khul@outlook.cz>
2024-10-26 12:46:10 +0200

    new window cration

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 import tkinter
-print("Hello world")
 
-repeats = input("How many times you want to print hello world? ")
-for i in range(int(repeats)):
-    print("Hello world")
+class CarFuelTracker:
+    def __init__(self, window):
+        self.window = window
+        self.window.title("Cool Fuel Tracker")
+        self.window.geometry("400x500")
+
+        # Create a menu
+        menu_bar = tkinter.Menu(window)
+        fuel_menu = tkinter.Menu(menu_bar, tearoff = 0)
+        fuel_menu.add_command(label = "New tanking")
+        fuel_menu.add_command(label = "Fuel statistics")
+        menu_bar.add_cascade(label = "Fuel", menu = fuel_menu)
+
+        service_menu = tkinter.Menu(menu_bar, tearoff = 0)
+        service_menu.add_command(label = "New service")
+        service_menu.add_command(label = "Service statistics")
+        menu_bar.add_cascade(label = "Service", menu = service_menu)
+
+        costs_menu = tkinter.Menu(menu_bar, tearoff = 0)
+        costs_menu.add_command(label = "New cost")
+        costs_menu.add_command(label = "Cost statistics")
+        menu_bar.add_cascade(label = "Costs", menu = costs_menu)
+
+        window.config(menu = menu_bar)
+    
+    
+    
+
+if __name__ == "__main__":
+    window = tkinter.Tk()
+    app = CarFuelTracker(window)
+    window.mainloop()
